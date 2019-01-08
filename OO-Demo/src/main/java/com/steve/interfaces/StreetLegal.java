b2993600706362f352bd5e81a494f7af4f00518e
Steve Keiser <steve@kdata.us>
2019-01-07 22:46:24 -0700

    Updated StreetLegal interface

--- OO-Demo/src/main/java/com/steve/interfaces/StreetLegal.java
+++ OO-Demo/src/main/java/com/steve/interfaces/StreetLegal.java
@@ -1,5 +1,8 @@
 package com.steve.interfaces;
 
 public interface StreetLegal {
-
+	public void runHornTest();
+	public void runLightTest();
+	public void runBrakeTest();
+	public void measureEmissions();
 }
